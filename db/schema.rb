6b70611e42cc1a472546ed78e6dcaf8dbac51cbb
Basil Dlamini <basil.csc@gmail.com>
2026-01-13 12:16:57 +0200

    added Tasks Model via scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_13_101135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "tasks", force: :cascade do |t|
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 end
